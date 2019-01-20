165a9ba25d299511ac420c024f1ed35e3133d908
Andrew Dansby <andydansby@gmail.com>
2019-01-20 07:38:58 -0500

    Add files via upload

--- gamedisplay.h
+++ gamedisplay.h
@@ -2,17 +2,9 @@
 #ifndef GAMEDISPLAY_H
 #define GAMEDISPLAY_H
 
-#define max(x,y) (((x)>(y))?(x):(y))
-#define min(x,y) (((x)<(y))?(x):(y))
+#include "gamedisplay1.h"
 
-#define printInk(k)			printf("\x10%c", (k))
-#define printPaper(k)		printf("\x11%c", (k))
-#define printMode(mode)		printf("\x01%c", (mode))
-#define printAt(row, col)	printf("\x16%c%c", ' '+(row), ' '+(col))
-#define FLASH_ON			printf("\x12\x31")
-#define printCls()			printf("\x0c")
-#define printInk(k)			printf("\x10%c", (k))
-#define printPaper(k)		printf("\x11%c", (k))
+
 
 //these must be public to pass onto _show_str
 //int _g_number;
@@ -30,149 +22,6 @@
   16384-22528 pixel data
 */
 
-	void cls()
-	{		
-		zx_border(0);
-		printPaper(0);
-		printInk(2);
-		printCls();
-	}
-
-	void z88dkPrint(unsigned char *string, unsigned char x, unsigned char y)
-	{
-		printMode(32);//how many columns
-		printAt(x, y);
-		printf(string);//printf("GAME OVER");
-	}
-
-void clearScreen()
-{
-	//clear the sprites
-	for (screenCounter = 0; screenCounter < 5; ++ screenCounter )
-	{
-		sprites[screenCounter].n = 129;
-	}
-	
-	for (screenScanY = 0; screenScanY < 10; ++ screenScanY)
-	{
-		for (screenScanX = 0; screenScanX < 15; ++ screenScanX)
-		{
-			tiles[screenScanY * scrw + screenScanX] = 0;
-		}
-	}
-	//#define tilepaint(from_x, from_y, to_x, to_y)
-	tilepaint(0, 0, 14, 9) | *shadow << 8;
-	FRAME;
-}
-
-	void blockClear()
-	{
-		Bitmap(1, 0);
-		Bitmap(1, 1);
-		Bitmap(1, 2);
-	}
-
-	void insertionSort()
-	{
-		char hiTemp[4];
-		
-		i = 0;
-		j = 0;
-		
-		for(i = 1; i < 6; ++i)
-		{
-			key = scores[i].hiScoreList;
-			
-			strcpy(hiTemp, scores[i].hiInitals);//copies the value of initials[i] to hiTemp
-			j = i - 1;
-			
-			
-			while( key > scores[j].hiScoreList && j >= 0)
-			{
-				//swap scores
-				scores[j + 1].hiScoreList = scores[j].hiScoreList;
-				
-				//copy initials
-				strcpy(scores[j + 1].hiInitals, scores[j].hiInitals);
-				
-				-- j;
-			}
-			
-			scores[j + 1].hiScoreList = key;// place temp value in slot
-			strcpy(scores[j + 1].hiInitals, hiTemp);
-		}
-	}
-	
-	void update_scoreboard()
-	{
-		ltoa(yourScore,yourScoreTXT , 10);//use lota for long variables
-		//PrintStr(yourScoreTXT, 0x0701);
-		PrintStr128(yourScoreTXT, 1,7);
-		//PrintStr(scoreTxT, 0x0101);
-		PrintStr128("Score", 1, 1);
-		
-		itoa(yourLives,yourLivesTXT , 10);
-		PrintStr(yourLivesTXT, 0x1901);
-		PrintStr(livesTxT, 0x1301);
-	}
-
-
-/*print_value
-void print_value(unsigned char x, unsigned char y, int number)
-{
-    _g_str_x = x;
-    _g_str_y = y;
-    _g_mask = 1;
-
-    //_itoa(number); // g_string saved in func
-	itoa(number, yourLivesTXT, 10);
-	textString = yourLivesTXT;
-
-    // print with engine sync
-    //if(fase_status == FASE_RUNNING)
-    //    *drwout = (unsigned int) _show_str;
-    //else
-    //    _show_str();
-}
-*/
-
-
-
-
-void update_screen()
-{
-	//*screen= mapy * mapw + mapx;
-	
-	*screen = gameMap;
-	
-	for ( j = 1; j < 5; ++j )
-	{
-		if( sprites[j].n > 0x7f )
-			sprites[j].n -= 0x80;
-	}
-}
-
-void displayStart()
-{
-	count = 30;	
-	for (iterator = 0; iterator < 3; ++ iterator )
-	{
-		PrintStr(gameStartTXT, 0x1001);
-		Pause (count);
-		PrintStr(clearTXT, 0x1001);
-		Pause (count);
-		count = count - 10;
-		
-		//print_str(unsigned char x, unsigned char y, unsigned char mask, unsigned char *s)
-		
-		//beepfx(0);
-		//FRAME;
-		//Sound(EFFX, 6);
-		//#asm 
-		//ld hl,2 
-		//#endasm		
-	}
-}
 
 
 void printHighScores()
@@ -180,66 +29,125 @@ void printHighScores()
 	screenCounter = 0;	
 	insertionSort();
 	
-	//PrintStr128(highScoreText, 01, 01);
-	//FRAME;
-	//z88dkPrint(highScoreText, 9, 6);
-	//z88dkPrint(highScoreText, 9, 6) | *shadow << 8;
-	PrintStr128(highScoreText, 6, 9);	
+	PrintStr(highScoreText, 0x0609);// 9, 5
 	
-	xx = 11;//xx = 0x0403;
+	//xx = 11;//xx = 0x0403;
+	xx = 0x040b;
 	
 	for (screenCounter = 0; screenCounter < 5; screenCounter++ )
 	{
-		strcpy (str," ");
+		strcpy (str,"          ");
 		strcat (str,scores[screenCounter].hiInitals);	//Concatenate strings
 		strcat (str,"....");
 		
 		ltoa(scores[screenCounter].hiScoreList, yourScoreTXT , 10);//use lota for long variables
 		strcat (str,yourScoreTXT);
 		
-		PrintStr128(str, 9, xx);		
+		//PrintStr128(str, 9, xx);
+		PrintStr(str, xx);
+		
 		xx ++;		
 	}	
 }
 
-void highScore()
+
+/*void _show_str(unsigned char *s, unsigned char g_str_y, unsigned char g_str_x)
 {
-	letter = 65;//starting at the letter A
-	arrayCounter = 0;
+    //unsigned char * s = g_string;
 
-	if (speccy128k == true){
-		//EXIT;
-	}
-	
-	#asm
-		ld hl,$5c01
-		ld a,127
-		ld (hl),a
-		ld a,0
-		ld (hl),a		
-	#endasm
-	
-	PrintStr128("CONGRATULATIONS", 9, 5);
-	PrintStr128("YOU HAVE A NEW HIGH SCORE", 4,7);
-	
-	PrintStr128("Enter your initals with", 2,17);// 
-	PrintStr128("UP/Down/FIRE", 9,18);
+    // no fona para mayores de 8
+    if( g_str_y < 9)
+        dst= (g_str_y * 0x20 ) + g_str_x + 0x4000 | *shadow << 8;
+    else
+        dst= ((g_str_y - 9 ) * 0x20 ) + g_str_x + 0x5000 | *shadow << 8;
 
-//	firstInital = 65;//letter A
-//	secondInital = 65;//letter A
-//	thirdInital = 65;//letter A
-		
-/*	
-	while (1)
+    
+   while (*s)   {
+       scr= 0x3c00 + *s++ * 8;
+       
+        for ( count= 0; count < 8; count++ )
+        {
+            zxmem[dst] = zxmem[scr++] ^ 0xff;
+            dst+= 0x100;
+        }
+
+        dst-= 0x7ff;
+
+    }
+}*/
+
+void printToMemory(unsigned char x, unsigned char y, unsigned char letter)
+{
+	*zxmem = 0;
+	
+	//0x3e08 = 15880 = letter A
+	scr = 15872 + letter * 8;
+	//dst = zx_cyx2saddr(y,x+i);
+	
+	if (speccy128k == true)
+		dst = zx_cyx2saddr(y,x+i)|*shadow << 8;
+	if (speccy128k == false)
+		dst = zx_cyx2saddr(y,x+i);
+	
+	for ( count= 0; count < 8; count++ )
 	{
-		//now we have down doing the same as up for testing
+		zxmem[dst] = zxmem[scr++]^0xff,
+		dst+= 0x100;
+	}
+}
+
+void highscoreEntry()
+{
+	unsigned char initial_1, initial_2, initial_3 = 65;
+	arrayCounter = 0;
+	letter = 1;//letter = 32; letter = 65;
+	
+	//while (1)
+	while (arrayCounter < 3)
+	{
+
+		//letter tiles are 32 to 57
+		//however sinclair ascii is from 65 to 90
+		//if (letter > 90) letter = 65;//if you go past Z, roll back to A
+		//if (letter < 65) letter = 90;//if you go past A, roll back to Z
+		
+		if (letter > 26) letter = 1;//if you go past Z, roll back to A
+		if (letter < 1) letter = 26;//if you go past A, roll back to Z
+
+		//initials[arrayCounter] = letter;
+		//sprintf(buffer, "%c", letter);
+		
+		//tilepaint(0x0d, 0x0e, 0x0f, 0x0e);
+		
+		if (arrayCounter == 0)
+		{
+			initial_1 = letter;
+			//PrintStr(&initial_1, 0x0D0E);
+			//PrintStr(buffer, 0x0D0E);			
+			//PrintStr((unsigned char*)initial_1, 0x0D0E);
+			printToMemory(0x9, 0x0e, initial_1);
+
+		}
+		
+		if (arrayCounter == 1)
+		{
+			initial_2 = letter;
+			printToMemory(0x0A, 0x0e, initial_2);
+		}
+		
+		if (arrayCounter == 2)
+		{
+			initial_3 = letter;
+			printToMemory(0x0B, 0x0e, initial_3);
+		}		
+	
 		if( Input() & 0x04 )// A go down
 		{
-			++ letter;
+			-- letter;
 		}
 		if( Input() & 0x08 )// Q go up
 		{
-			-- letter;			
+			++ letter;			
 		}
 		if( Input() & 0x10) // Space (FIRE)
 		{
@@ -248,99 +156,93 @@ void highScore()
 			++ arrayCounter;
 		}
 		
-		if (letter > 90) letter = 65;//if you go past Z, roll back to A
-		if (letter < 65) letter = 90;//if you go past A, roll back to Z
-
-		initials[arrayCounter] = letter;
+		Pause (10);
 		
-		sprintf(buffer, "%c", letter);
-		
-		if (arrayCounter == 0)
-		{
-			z88dkPrint(buffer, 20,12);
-			//firstInital = buffer;
-		}		
-		if (arrayCounter == 1)
-		{
-			z88dkPrint(buffer, 20,13);
-			//secondInital = buffer;
-		}		
-		if (arrayCounter == 2)
-		{
-			z88dkPrint(buffer, 20,14);
-			//thirdInital = buffer;
-		}		
 		if (arrayCounter == 3)
-		{	break;}
-		
-		//FRAME;
-		Pause (150);
+		{	
+			break;
+		}
 	}
-*/
+	//scores[5].hiInitals[0] = (initials[0]);
+	//scores[5].hiInitals[1] = (initials[1]);
+	//scores[5].hiInitals[2] = (initials[2]);
 	
-	scores[5].hiInitals[0] = (initials[0]);
-	scores[5].hiInitals[1] = (initials[1]);
-	scores[5].hiInitals[2] = (initials[2]);
+	//scores[5].hiInitals[0] = initial_1;
+	//scores[5].hiInitals[1] = initial_2;
+	//scores[5].hiInitals[2] = initial_3;
+	
+	//+64 to adjust to sinclair ascii
+	scores[5].hiInitals[0] = initial_1 + 64;
+	scores[5].hiInitals[1] = initial_2 + 64;
+	scores[5].hiInitals[2] = initial_3 + 64;
 	scores[screenCounter].hiScoreList = yourScore;
+}
+
+
+
+void highScore()
+{
+	assemblyCLS();
+
+	for ( x = 0; x < 200; ++ x )//approx 3 seconds 120
+	{
+		//if (speccy128k == false)
+		{
+			PrintStr(highScoreMessage1, 0x0905);// 9, 5
+			PrintStr(highScoreMessage2, 0x0407);// 4,7
+			PrintStr(highScoreMessage3, 0x0411);// 2,17
+			PrintStr(highScoreMessage4, 0x0a12);//9,18
+		}
+	}
+
+	//now we enter the hiscore initals
+	highscoreEntry();
 
 	//sort high score table
 	insertionSort();
-	
-	//print high score table
-	//printHighScores();
 }
 
-
-
-
-
-//notes
-//need to rewrite end game to only have 1 frame at the end
-
-// special attention needs to be given during high score print
-// and high score entry
-
-//You must add shadow<<8 to the address before write because in 128K mode there is another screen between 0xc000 and 0xdaff. 
-	//| *shadow << 8;
-	//dst= 0x50de | *shadow << 8;
-	//z88dkPrint("GAME OVER", 3,11);
-	//z88dkPrint("GAME OVER", 3,11) | *shadow << 8;
 // Game Over
-
-
 void endgame()
 {
-	
-	clearScreen();
-
-	//check to see if your current score is a new high score
-	if (yourScore > scores[4].hiScoreList)
+	assemblyCLS();
 	{
-		//if (speccy128k == true){			
-		//}
-		highScore();
+		PrintStr(gameOverTXT, 0x0b03);
 	}
-	else
-	{
-		//FRAME;//only use one FRAME at the very end
-		//PrintStr128("GAME OVER", 11, 3);
-		//z88dkPrint("GAME OVER", 3,11);
-		#asm
-			ld hl,$5c01
-			ld a,0
-			ld (hl),a
-		#endasm
-
-		PrintStr128("Game Over", 11, 3);
-		
-	}
-	
-	printHighScores();
-	
-	FRAME;
-	Pause (300);//Pause (120);
+	Pause (180);//Pause (120);
 }
 
+
+
+
+//via loader.asm
+//ld a, ($5c01) ; toggle port value between 00 and 80 every frame
+
+
+//via engine.asm
+//ld a, (port&$ffff) ; toggle port value between 00 and 80 every frame
+//xor $80
+//ld (port&$ffff), a
+//ld a, $18 ; also toggle between bank 5 & 7 for the screen
+// jr z, do2 ; and 7 & 0 for the current paging at $c000
+//dec a ; so we always show a screen and modify the other
+
+//via engine
+//; Initialisation code. The user call this routine to start the engine
+//; This will paint clipping bars
+//; In 48k also paint the sync bar
+//; In 128k we copy from RAM bank 0 to RAM bank 7
+
+
+
+
+
+
+	
+//http://mdfs.net/Info/Comp/Spectrum/ProgTips/Printout/
+
+
+//https://www.worldofspectrum.org/forums/discussion/51198/
 
 
 
